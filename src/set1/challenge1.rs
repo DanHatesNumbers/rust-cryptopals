f08dff3142a254340b5c455e1d78cc25e138f2b8
Daniel Murphy <danwright91@gmail.com>
2017-08-01 08:51:21 +0100

    Added solution to set 1 challenge 1

--- src/set1/challenge1.rs
+++ src/set1/challenge1.rs
@@ -1,3 +1,11 @@
+extern crate data_encoding;
+use self::data_encoding::{BASE64, HEXLOWER};
+
 pub fn solution() {
-    ()
+    let input = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+    let actual_output = BASE64.encode(&HEXLOWER.decode(input).unwrap());
+
+    assert_eq!(expected_output, actual_output);
 }
