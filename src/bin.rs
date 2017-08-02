ed93845d62de973b638a021f8c3f6258c989a8a8
Daniel Murphy <danwright91@gmail.com>
2017-08-02 15:41:30 +0100

    Added function to perform repeating key xor for set 1 challenge 3.

--- src/bin.rs
+++ src/bin.rs
@@ -3,4 +3,5 @@ extern crate cryptopals_lib;
 pub fn main() {
     cryptopals_lib::set1::challenge1::solution();
     cryptopals_lib::set1::challenge2::solution();
+    cryptopals_lib::set1::challenge3::solution();
 }
---
+++ src/set1/challenge3.rs
@@ -0,0 +1,24 @@
+extern crate data_encoding;
+use self::data_encoding::HEXLOWER;
+
+pub fn solution() {
+    let input = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let input_bytes: Vec<u8> = HEXLOWER.decode(input).unwrap();
+
+    let candidate_keys: Vec<Vec<u8>> = (0..26).map(|x| vec![x + 'a' as u8]).collect::<Vec<Vec<_>>>();
+
+    let candidate_solutions = candidate_keys.iter()
+        .map(|x| repeating_key_xor(input_bytes.iter(), x.iter()))
+        .collect::<Vec<_>>();
+
+    println!("{:?}", HEXLOWER.encode(&candidate_solutions[1]));
+}
+
+fn repeating_key_xor<'a, 'b, T, U>(input_bytes: T, key_bytes: U) -> Vec<u8> 
+where T: Iterator<Item=&'a u8>, U: Iterator<Item=&'b u8> + Clone
+{
+    input_bytes
+        .zip(key_bytes.cycle())
+        .map(|(input_byte, key_byte)| input_byte ^ key_byte)
+        .collect::<Vec<_>>()
+}
