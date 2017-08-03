998916831deeb9fc2e7716a70e808f2ec0de4c80
Daniel Murphy <danwright91@gmail.com>
2017-08-03 14:33:24 +0100

    Added plaintext scoring and a solution to set 1 challenge 3. Prints plaintext a bit weird, but it's the only candidate plaintext resembling english.

--- src/set1/challenge3.rs
+++ src/set1/challenge3.rs
@@ -1,5 +1,8 @@
+extern crate ascii;
 extern crate data_encoding;
+use self::ascii::{AsciiString, AsciiStr, AsciiChar};
 use self::data_encoding::HEXLOWER;
+use std::ascii::AsciiExt;
 
 pub fn solution() {
     let input = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
@@ -7,9 +10,16 @@ pub fn solution() {
 
     let candidate_keys: Vec<Vec<u8>> = (0..26).map(|x| vec![x + 'a' as u8]).collect::<Vec<Vec<_>>>();
 
-    let candidate_solutions = candidate_keys.iter()
-        .map(|x| String::from_utf8(repeating_key_xor(input_bytes.iter(), x.iter())))
-        .collect::<Vec<_>>();
+    let candidate_solution = candidate_keys.iter()
+        .map(|x| {
+            let candidate_plaintext = AsciiString::from_ascii(repeating_key_xor(input_bytes.iter(), x.iter())).unwrap();
+            let encoded_key = AsciiString::from_ascii(x.clone()).unwrap();
+            let plaintext_score = plaintext_scoring(&candidate_plaintext);
+            CandidateSolution {score: plaintext_score, plaintext: candidate_plaintext, key: encoded_key}
+        })
+        .max_by_key(|x| x.score).unwrap();
+
+    println!("{:?}", candidate_solution);
 }
 
 fn repeating_key_xor<'a, 'b, T, U>(input_bytes: T, key_bytes: U) -> Vec<u8> 
@@ -20,3 +30,52 @@ where T: Iterator<Item=&'a u8>, U: Iterator<Item=&'b u8> + Clone
         .map(|(input_byte, key_byte)| input_byte ^ key_byte)
         .collect::<Vec<_>>()
 }
+
+fn plaintext_scoring(plaintext: &AsciiStr) -> isize {
+    plaintext
+        .to_ascii_lowercase()
+        .chars()
+        .map(|x| 
+        {
+            match *x
+            {
+                AsciiChar::e => 26,
+                AsciiChar::t => 25,
+                AsciiChar::a => 24,
+                AsciiChar::o => 23,
+                AsciiChar::i => 22,
+                AsciiChar::n => 21,
+                AsciiChar::s => 20,
+                AsciiChar::r => 19,
+                AsciiChar::h => 18,
+                AsciiChar::l => 17,
+                AsciiChar::d => 16,
+                AsciiChar::c => 15,
+                AsciiChar::u => 14,
+                AsciiChar::m => 13,
+                AsciiChar::f => 12,
+                AsciiChar::p => 11,
+                AsciiChar::g => 10,
+                AsciiChar::w => 9,
+                AsciiChar::y => 8,
+                AsciiChar::b => 7,
+                AsciiChar::v => 6,
+                AsciiChar::k => 5,
+                AsciiChar::x => 4,
+                AsciiChar::j => 3,
+                AsciiChar::q => 2,
+                AsciiChar::z => 1,
+                _ => 0
+            }
+        })
+    .sum()
+}
+
+#[derive(Debug)]
+struct CandidateSolution
+{
+    score: isize,
+    key: AsciiString,
+    plaintext: AsciiString
+}
+
