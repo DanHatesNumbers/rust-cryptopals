ccdcae562d555ce68a177d31db504cfd564de3ae
Daniel Murphy <danwright91@gmail.com>
2017-08-04 13:50:49 +0100

    Refactored expression for generating ascii printable char bytes into a helper func.
    Refactored plaintext_scoring function to accept a reference to an Option<AsciiString> to account for unencodable plaintexts.
    
    Remove use of unwrapping the result of from_ascii in favour of using the Result::ok function to convert a Result into an Option to handle candidate plaintexts that aren't valid ASCII strings in challenge 3.

--- src/set1/challenge3.rs
+++ src/set1/challenge3.rs
@@ -8,11 +8,12 @@ pub fn solution() {
     let input = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     let input_bytes: Vec<u8> = HEXLOWER.decode(input).unwrap();
 
-    let candidate_keys: Vec<Vec<u8>> = (32..126).map(|x| vec![x as u8]).collect::<Vec<Vec<_>>>();
+    let candidate_keys: Vec<Vec<u8>> = ascii_printable_chars_bytes();
 
     let candidate_solution = candidate_keys.iter()
         .map(|x| {
-            let candidate_plaintext = AsciiString::from_ascii(repeating_key_xor(input_bytes.iter(), x.iter())).unwrap();
+            let candidate_plaintext_result = AsciiString::from_ascii(repeating_key_xor(input_bytes.iter(), x.iter()));
+            let candidate_plaintext = candidate_plaintext_result.ok();
             let encoded_key = AsciiString::from_ascii(x.clone()).unwrap();
             let plaintext_score = plaintext_scoring(&candidate_plaintext);
             CandidateSolution {score: plaintext_score, plaintext: candidate_plaintext, key: encoded_key}
--- src/set1/helpers.rs
+++ src/set1/helpers.rs
@@ -1,6 +1,7 @@
 extern crate ascii;
 extern crate data_encoding;
 use self::ascii::{AsciiString, AsciiStr, AsciiChar};
+use std::cmp::Ordering;
 use self::data_encoding::HEXLOWER;
 use std::ascii::AsciiExt;
 
@@ -13,45 +14,54 @@ where T: Iterator<Item=&'a u8>, U: Iterator<Item=&'b u8> + Clone
         .collect::<Vec<_>>()
 }
 
-pub fn plaintext_scoring(plaintext: &AsciiStr) -> isize {
-    plaintext
-        .to_ascii_lowercase()
-        .chars()
-        .map(|x| 
-        {
-            match *x
-            {
-                AsciiChar::e => 26,
-                AsciiChar::t => 25,
-                AsciiChar::a => 24,
-                AsciiChar::o => 23,
-                AsciiChar::i => 22,
-                AsciiChar::n => 21,
-                AsciiChar::s => 20,
-                AsciiChar::r => 19,
-                AsciiChar::h => 18,
-                AsciiChar::l => 17,
-                AsciiChar::d => 16,
-                AsciiChar::c => 15,
-                AsciiChar::u => 14,
-                AsciiChar::m => 13,
-                AsciiChar::f => 12,
-                AsciiChar::p => 11,
-                AsciiChar::g => 10,
-                AsciiChar::w => 9,
-                AsciiChar::y => 8,
-                AsciiChar::b => 7,
-                AsciiChar::v => 6,
-                AsciiChar::k => 5,
-                AsciiChar::x => 4,
-                AsciiChar::j => 3,
-                AsciiChar::q => 2,
-                AsciiChar::z => 1,
-                AsciiChar::Space => 1,
-                _ => -1
-            }
-        })
-    .sum()
+pub fn plaintext_scoring(plaintext: &Option<AsciiString>) -> isize {
+    match *plaintext {
+        Some(ref plaintext) => {
+            plaintext
+                .to_ascii_lowercase()
+                .chars()
+                .map(|x| 
+                {
+                    match *x
+                    {
+                        AsciiChar::e => 26,
+                        AsciiChar::t => 25,
+                        AsciiChar::a => 24,
+                        AsciiChar::o => 23,
+                        AsciiChar::i => 22,
+                        AsciiChar::n => 21,
+                        AsciiChar::s => 20,
+                        AsciiChar::r => 19,
+                        AsciiChar::h => 18,
+                        AsciiChar::l => 17,
+                        AsciiChar::d => 16,
+                        AsciiChar::c => 15,
+                        AsciiChar::u => 14,
+                        AsciiChar::m => 13,
+                        AsciiChar::f => 12,
+                        AsciiChar::p => 11,
+                        AsciiChar::g => 10,
+                        AsciiChar::w => 9,
+                        AsciiChar::y => 8,
+                        AsciiChar::b => 7,
+                        AsciiChar::v => 6,
+                        AsciiChar::k => 5,
+                        AsciiChar::x => 4,
+                        AsciiChar::j => 3,
+                        AsciiChar::q => 2,
+                        AsciiChar::z => 1,
+                        AsciiChar::Space => 1,
+                        _ => -1
+                    }
+                })
+            .sum()
+        },
+        None => 0
+    }
+}
+
+pub fn ascii_printable_chars_bytes() -> Vec<Vec<u8>> {
+    (32..126).map(|x| vec![x as u8]).collect::<Vec<Vec<_>>>()
 }
 
 #[derive(Debug)]
@@ -59,5 +69,25 @@ pub struct CandidateSolution
 {
     pub score: isize,
     pub key: AsciiString,
-    pub plaintext: AsciiString
+    pub plaintext: Option<AsciiString>
 }
+
+impl Ord for CandidateSolution {
+    fn cmp(&self, other: &CandidateSolution) -> Ordering {
+        self.score.cmp(&other.score)
+    }
+}
+
+impl PartialOrd for CandidateSolution {
+    fn partial_cmp(&self, other: &CandidateSolution) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for CandidateSolution {
+    fn eq(&self, other: &CandidateSolution) -> bool {
+        self.score == other.score
+    }
+}
+
+impl Eq for CandidateSolution {}
